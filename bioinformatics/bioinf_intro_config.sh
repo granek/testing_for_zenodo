b839e106f3f5799bfb1d2f61b30afd46cd508ef7
Josh Granek <josh@duke.edu>
2019-06-21 22:10:29 +0000

    everything_together.ipynb: New! Some modifications were made to bioinf_intro_config.sh to accomodate

--- bioinformatics/bioinf_intro_config.sh
+++ bioinformatics/bioinf_intro_config.sh
@@ -10,9 +10,15 @@ export TRIMMED=$CUROUT/trimmed_fastqs
 export MYINFO=$CUROUT/myinfo
 export GENOME_DIR=$CUROUT/genome
 export STAR_OUT=$CUROUT/star_out
-export FASTA=${GENOME_DIR}/Cryptococcus_neoformans_var_grubii_h99.CNA3.dna.toplevel.fa
-export GTF=${GENOME_DIR}/Cryptococcus_neoformans_var_grubii_h99.CNA3.39.gtf
 export COUNT_OUT=$CUROUT/count_out
-export QC=$CUROUT/qc_output
+export QC_RAW=$CUROUT/qc_output_raw
+export QC_TRIM=$CUROUT/qc_output_trim
 export IGV_DIR="$CUROUT/igv"
 export ADAPTERS=$MYINFO/neb_e7600_adapters.fasta
+export THREADS=2
+
+# Genome
+export FA_URL="ftp://ftp.ensemblgenomes.org/pub/release-39/fungi/fasta/fungi_basidiomycota1_collection/cryptococcus_neoformans_var_grubii_h99/dna/Cryptococcus_neoformans_var_grubii_h99.CNA3.dna.toplevel.fa.gz"
+export GTF_URL="ftp://ftp.ensemblgenomes.org/pub/release-39/fungi/gtf/fungi_basidiomycota1_collection/cryptococcus_neoformans_var_grubii_h99/Cryptococcus_neoformans_var_grubii_h99.CNA3.39.gtf.gz"
+export GTF=$(basename ${GTF_URL%.gz})
+export FA=$(basename ${FA_URL%.gz})
